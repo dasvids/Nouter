4572979f260f0a5f40fd513050e24b9e51932582
grchs <nedorubartem@gmail.com>
2022-01-04 19:41:30 +0300

    Added create for notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -9,9 +9,28 @@ class NotesController < ApplicationController
     render_not_found unless @note
   end
 
+  def new
+    @note = Note.new
+  end
+
+  def create
+    note = Note.create note_params
+    unless note.valid?
+      return redirect_to new_note_path,
+                         alert: note.errors.full_messages
+    end
+    redirect_to note_path(note)
+  end
+
+
   private
 
   def set_note
     @note = Note.find_by_id(params[:id])
   end
+
+  def note_params
+    params.require(:note).permit(:title, :text)
+  end
+
 end
