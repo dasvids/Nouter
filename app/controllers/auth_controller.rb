2398f6e8a28372614b3d6c1c35f8450192c4ca0c
grchs <nedorubartem@gmail.com>
2022-01-07 12:16:32 +0300

    Fixed auth create

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,8 +5,8 @@ class AuthController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:user][:email])
-    if @user.password == params[:password]
+    @user = User.find_by_email(user_params[:email])
+    if @user.authenticate(user_params[:password])
       session[:user_id] = @user.id
       redirect_to feed_path
     else
@@ -17,5 +17,11 @@ class AuthController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to feed_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
