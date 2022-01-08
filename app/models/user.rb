e67bd7fedc01b393e821825304d05265886cdb0f
grchs <nedorubartem@gmail.com>
2022-01-08 19:08:38 +0300

    something

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,24 @@ class User < ApplicationRecord
 
   has_many :notes
 
-  has_secure_password
+  has_secure_password validations: false
+
+  validate :password_presence
+
+  validates :password, confirmation: true, allow_blank: true
+
+  validate :password_complexity
+
+  private
+
+  def password_complexity
+    # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+    return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
+
+    errors.add :password, 'Complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
+
+  def password_presence
+    errors.add(:password, :blank) unless password_digest.present?
+  end
 end
